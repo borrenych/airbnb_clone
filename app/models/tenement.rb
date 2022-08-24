79d47b811655033125201e2f7b2e7eb403ef9024
borrenych <darthflackojodye@gmail.com>
2022-08-24 15:53:04 +0300

    Tenement validation

--- app/models/tenement.rb
+++ app/models/tenement.rb
@@ -2,4 +2,5 @@ class Tenement < ApplicationRecord
   belongs_to :property_type
   has_many :bookings, dependent: :destroy
   belongs_to :user
+  validates :title, :description, :price, :guests, :region, :property_type_id, :user_id, presence: true
 end
