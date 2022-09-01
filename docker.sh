dfe7f3aad06ec2e92246ffa87d2135809cc18426
borrenych <darthflackojodye@gmail.com>
2022-09-01 17:04:17 +0300

    webpacker:compile to docker.sh

--- docker.sh
+++ docker.sh
@@ -3,9 +3,10 @@ set -e
 
 rm -f tmp/pids/server.pid
 
-yarn install
 bundle install
 bundle exec rails db:create
 bundle exec rails db:migrate
+
+bundle exec rails webpacker:compile
 
 bundle exec rails server -p 3000 -b '0.0.0.0'
