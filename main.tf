d4f61e971e2ac2877bb01dd0f7b628a25d5c65f6
Никита Шелаев <42943483+ShelaevNikita@users.noreply.github.com>
2023-01-08 22:01:06 +0300

    YTOKEN

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "yandex" {
-	token     = ${{ secrets.Ytoken }}
+	token     = ${{ secrets.YTOKEN }}
 	cloud_id  = var.yandex_cloud_id
 	folder_id = var.yandex_folder_id
 	zone 	  = var.yandex_zone
