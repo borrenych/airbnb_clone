c8145a7d2e0af06abf6f8358632ea2715034a205
Никита Шелаев <42943483+ShelaevNikita@users.noreply.github.com>
2023-01-09 00:27:20 +0300

    final files

--- main.tf
+++ main.tf
@@ -51,5 +51,5 @@ resource "yandex_vpc_subnet" "subnet_terraform" {
   name           = var.yandex_subnet
   zone           = var.yandex_zone
   network_id     = yandex_vpc_network.network_terraform.id
-  v4_cidr_blocks = ["10.128.0.0/24"]
+  v4_cidr_blocks = ["128.198.0.0/24"]
 }
